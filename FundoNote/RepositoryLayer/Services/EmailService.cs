fa21835c170ee3bc713e757801c12e471c6b20a5
Mahammed Arshad Qureshi <31272258+Mohammed-Arshad-Qureshi@users.noreply.github.com>
2022-07-08 16:58:35 +0530

    Update EmailService.cs

--- FundoNote/RepositoryLayer/Services/EmailService.cs
+++ FundoNote/RepositoryLayer/Services/EmailService.cs
@@ -15,7 +15,7 @@ namespace RepositoryLayer.Services
                 client.EnableSsl = true;
                 client.DeliveryMethod = SmtpDeliveryMethod.Network;
                 client.UseDefaultCredentials = true;
-                client.Credentials = new NetworkCredential("testingEmailarshad@gmail.com", "rcwbvvjwpenwmtxb"); //Change to your created email and generated password
+                client.Credentials = new NetworkCredential("testingEmailarshad@gmail.com", "rcwbvvjwpenwmtxb");
                 MailMessage msgObj = new MailMessage();
                 msgObj.To.Add(email);
                 msgObj.IsBodyHtml = true;
